582f43ef7230bec2b297ba15d99f782b356ed506
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-11-27 23:27:02 -0600

    updated cors to accept netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://angry-jones-963184.netlify.app/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :delete, :options],
