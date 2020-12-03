45bea3a957b8a9b43a24588d1b6fac75e3c27e62
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-12-03 14:47:05 -0600

    changed front end url and updated cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'https://angry-jones-963184.netlify.app']
+    origins ['http://localhost:3000', 'https://local-choss-climbing-locator.netlify.app']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :delete, :options],
